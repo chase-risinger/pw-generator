483d4a3b46be5b542c0e4a2e7dc0c8001bd73da1
chase-risinger <ccr1988@gmail.com>
2021-10-05 10:54:09 -0500

    Added more special characters

--- Develop/script.js
+++ Develop/script.js
@@ -4,7 +4,7 @@ var generateBtn = document.querySelector("#generate");
 var lettersLower = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var lettersUpper = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 var numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
-var specChars = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '+', '=', '[', ']', '{', '}', '/'];
+var specChars = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '+', '=', '[', ']', '{', '}', '/', ',', '.', '?'];
 var pwPool = [];
 var includeLower = false;
 var includeUpper = false;
